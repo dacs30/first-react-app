f603ae1ed3c24dafc8aeaf6a136286bc181be060
dacs2 <dacs2012@gmail.com>
2020-08-03 15:02:11 -0300

    Updates

---
+++ precache-manifest.28bced600045751ca55483a04c147e89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39a50d6b384db11ef16a2f7cf7c56cf",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "5fe1e5e584c753c3735c",
+    "url": "/first-react-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "575197e4be7539fbc1dc",
+    "url": "/first-react-app/static/css/main.4a083df8.chunk.css"
+  },
+  {
+    "revision": "5fe1e5e584c753c3735c",
+    "url": "/first-react-app/static/js/2.82e4e86b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.82e4e86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575197e4be7539fbc1dc",
+    "url": "/first-react-app/static/js/main.bfd84fbf.chunk.js"
+  },
+  {
+    "revision": "0f493befcf17eadfafb2",
+    "url": "/first-react-app/static/js/runtime-main.2b6e35ec.js"
+  }
+]);
